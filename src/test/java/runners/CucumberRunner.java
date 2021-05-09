16b938b1a93fbb0d4ea88ba8ca197ab2ddc6224b
meher28 <mehermeka@gmail.com>
2021-05-09 19:19:26 +0100

    reporters added

--- src/test/java/runners/CucumberRunner.java
+++ src/test/java/runners/CucumberRunner.java
@@ -5,6 +5,10 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources"}, glue = {"steps"})
+@CucumberOptions(features = {"src/test/resources"}, glue = {"steps"}
+        , plugin = {"pretty", "html:target/cucumber-reports/cucumber-pretty",
+        "json:target/cucumber-reports/CucumberTestReport.json",
+        "rerun:target/cucumber-reports/rerun.txt"},
+        monochrome = true)
 public class CucumberRunner {
 }
